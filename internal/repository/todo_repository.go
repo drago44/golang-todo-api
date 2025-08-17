20b5f9dec8ecbaa269ed821892fd2b5904994c78
Oleksandr <ku@grade.ua>
2025-08-17 16:29:56 +0300

    BE-0003 feat: enhance Todo functionality and update dependencies
    - Added TodoRepository interface and its implementation for CRUD operations.
    - Introduced TodoService for business logic related to Todo management.
    - Updated UpdateTodoRequest to use a pointer for the Completed field.
    - Removed the old service.go file and replaced it with a new todo_service.go file.
    - Updated go.mod to include GORM dependencies for database interactions.

--- internal/repository/todo_repository.go
+++ internal/repository/todo_repository.go
@@ -1,1 +1,53 @@
 package repository
+
+import (
+	"github.com/drago44/golang-todo-api/internal/models"
+	"gorm.io/gorm"
+)
+
+type TodoRepository interface {
+	Create(todo *models.Todo) error
+	GetAll() ([]models.Todo, error)
+	GetByID(id uint) (*models.Todo, error)
+	GetByTitle(title string) (*models.Todo, error)
+	Update(id uint, todo *models.Todo) error
+	Delete(id uint) error
+}
+
+type todoRepository struct {
+	db *gorm.DB
+}
+
+func NewTodoRepository(db *gorm.DB) TodoRepository {
+	return &todoRepository{db: db}
+}
+
+func (r *todoRepository) Create(todo *models.Todo) error {
+	return r.db.Create(todo).Error
+}
+
+func (r *todoRepository) GetAll() ([]models.Todo, error) {
+	var todos []models.Todo
+	err := r.db.Find(&todos).Error
+	return todos, err
+}
+
+func (r *todoRepository) GetByID(id uint) (*models.Todo, error) {
+	var todo models.Todo
+	err := r.db.First(&todo, id).Error
+	return &todo, err
+}
+
+func (r *todoRepository) GetByTitle(title string) (*models.Todo, error) {
+	var todo models.Todo
+	err := r.db.Where("title = ?", title).First(&todo).Error
+	return &todo, err
+}
+
+func (r *todoRepository) Update(id uint, todo *models.Todo) error {
+	return r.db.Save(todo).Error
+}
+
+func (r *todoRepository) Delete(id uint) error {
+	return r.db.Delete(&models.Todo{}, id).Error
+}
